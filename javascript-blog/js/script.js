621c33e5494beba03f757ffc6e4cde6eae8846c1
Mateusz Barej <mateusz.barej89@gmail.com>
2023-06-26 11:59:19 +0200

    add class 'active' to the clicked link

--- javascript-blog/js/script.js
+++ javascript-blog/js/script.js
@@ -1,30 +1,28 @@
 'use strict';
 
 function titleClickHandler(event){
-    event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked!');
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
+  /* [DONE] remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-console.log('clickedElement:', clickedElement);
-    clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-const activeArticles = document.querySelectorAll('.posts article.active');
+  /* [DONE] remove class 'active' from all articles */
 
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
+  /* get 'href' attribute from the clicked link */
 
+  /* find the correct article using the selector (value of 'href' attribute) */
 
-const articleSelector = clickedElement.getAttribute('href');
-
+  /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
