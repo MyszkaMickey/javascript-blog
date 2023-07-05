2fb2ceb1f0fe05aa9d61b782c29b46e3d5a44128
Mateusz Barej <mateusz.barej89@gmail.com>
2023-07-05 10:49:29 +0200

    Instal StyleLint

--- javascript-blog/js/script.js
+++ javascript-blog/js/script.js
@@ -1,34 +1,48 @@
 "use strict";
+
 function titleClickHandler(event) {
 	event.preventDefault();
-	const clickedElement = this;
-	console.log("Link was clicked!");
+  const clickedElement = this;
+  console.log("Link was clicked!");
+
 	/* [DONE] remove class 'active' from all article links */
 	const activeLinks = document.querySelectorAll(".titles a.active");
+
 	for (let activeLink of activeLinks) {
-		activeLink.classList.remove("active");
+		activeLink.classList.remove("active");}
 	}
+
 	/* [DONE] add class 'active' to the clicked link */
 	console.log("clickedElement:", clickedElement);
-	clickedElement.classList.add("active");
+  clickedElement.classList.add("active");
+
 	/* [DONE] remove class 'active' from all articles */
 	const activeArticles = document.querySelectorAll(".posts .active");
+
 	for (let activeArticle of activeArticles) {
 		activeArticle.classList.remove("active");
 	}
+
 	/* get 'href' attribute from the clicked link */
 	const articleSelector = clickedElement.getAttribute("href");
+
 	/* find the correct article using the selector (value of 'href' attribute) */
+
 	const targetArticle = document.querySelector(articleSelector);
+    
 	/* add class 'active' to the correct article */
 	targetArticle.classList.add("active");
 }
+
+
 const links = document.querySelectorAll(".titles a");
+console.log(links);
+
 for (let link of links) {
 	link.addEventListener("click", titleClickHandler);
 }
 
-/* tu zaczyna się nowe zadanie */
+/* New task- Generate title links */
 
 const optArticleSelector = ".post",
 	optTitleSelector = ".post-title",
@@ -42,15 +56,18 @@ function generateTitleLinks(customSelector = "") {
 	const titleList = document.querySelector(optTitleListSelector);
 	titleList.innerHTML = "";
 
-	/* [I'm not sure] for each article */
+	/* [done] for each article */
 
-	let html = " ";
+	let html = "";
 
 	const articles = document.querySelectorAll(
 		optArticleSelector + customSelector
 	);
 
+	console.log(articles, optArticleSelector + customSelector);
+
 	for (let article of articles) {
+
 		/* [done} get the article id */
 
 		const articleId = article.getAttribute("id");
@@ -60,14 +77,14 @@ function generateTitleLinks(customSelector = "") {
 
 		const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-		/* [I'm not sure] create HTML of the link */
+		/*[done]create HTML of the link */
 
 		const linkHTML =
-			'<li><a href="#' +
-			articleId +
-			'"><span>' +
-			articleTitle +
-			"</span></a></li>";
+		'<li><a href="#' +
+		articleId +
+		'"><span>' +
+		articleTitle +
+		"</span></a></li>";
 
 		console.log(linkHTML);
 
